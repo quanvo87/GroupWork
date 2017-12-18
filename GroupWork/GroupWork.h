24061cc1356a648a1b652ef7299f952f94063fbf
Quan Vo <qvo1987@gmail.com>
2017-12-18 15:31:21 -0600

    Initial Commit

---
+++ GroupWork/GroupWork.h
@@ -0,0 +1,19 @@
+//
+//  GroupWork.h
+//  GroupWork
+//
+//  Created by Quan Vo on 12/18/17.
+//  Copyright © 2017 Quan Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GroupWork.
+FOUNDATION_EXPORT double GroupWorkVersionNumber;
+
+//! Project version string for GroupWork.
+FOUNDATION_EXPORT const unsigned char GroupWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GroupWork/PublicHeader.h>
+
+
